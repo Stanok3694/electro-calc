45c03cd63d7cf6fc5d78a55e7bf944ffb8afad49
stanislav-okunev <Stanislav.Okunev@returnonintelligence.com>
2017-07-02 16:23:41 +0300

    release base functional

--- ElectricityPayments/Program.cs
+++ ElectricityPayments/Program.cs
@@ -1,15 +1,74 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ElectricityPayments
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
+            var serviceData = new ServiceFuncData();
+            var tarifs = new Tarifs();
+
+            var juneMonth = new CurrentMonth(2290, 1597);
+            var mayMonth = new PrevMonth(2160, 1521);
+
+            var dayDeltaBtwMonths = serviceData.CreateDelta(juneMonth.DayTop, mayMonth.DayTop);
+            var nightDeltaBtwMonths = serviceData.CreateDelta(juneMonth.NightTop, mayMonth.NightTop);
+
+            var daySummary = serviceData.FindPhaseSummary(dayDeltaBtwMonths, (int) SocialNormEnum.Day,
+                tarifs.SocialNormEqualDay, tarifs.SocialNormNotEqualDay);
+
+            var nightSumary = serviceData.FindPhaseSummary(nightDeltaBtwMonths, (int) SocialNormEnum.Night,
+                tarifs.SocialNormEqualNight, tarifs.SocialNormNotEqualNight);
+
+            juneMonth.Summary = serviceData.ResultSummary(daySummary, nightSumary);
+
+            Console.WriteLine("Your result is: " + juneMonth.Summary);
+            Console.ReadKey();
+        }
+
+        private sealed class Tarifs
+        {
+            public double SocialNormEqualDay { get; } = 3.41;
+            public double SocialNormEqualNight { get; } = 1.67;
+            public double SocialNormNotEqualDay { get; } = 6.59;
+            public double SocialNormNotEqualNight { get; } = 3.44;
+        }
+
+        internal enum SocialNormEnum
+        {
+            Day = 53, Night = 47
+        }
+
+        public interface IMonth
+        {
+            int DayTop { get; set; }
+            int NightTop { get; set; }
+        }
+
+        public class CurrentMonth : IMonth
+        {
+            public int DayTop { get; set; }
+            public int NightTop { get; set; }
+            public double Summary { get; set; }
+
+            public CurrentMonth(int dayTop, int nightTop)
+            {
+                DayTop = dayTop;
+                NightTop = nightTop;
+            }
+        }
+
+        public class PrevMonth:IMonth
+        {
+            public int DayTop { get; set; }
+            public int NightTop { get; set; }
+
+            public PrevMonth(int dayTop, int nightTop)
+            {
+                DayTop = dayTop;
+                NightTop = nightTop;
+            }
         }
     }
 }
---
+++ ElectricityPayments/ServiceFuncData.cs
@@ -0,0 +1,34 @@
+﻿namespace ElectricityPayments
+{
+    public class ServiceFuncData
+    {
+        public int CreateDelta(int a, int b)
+        {
+            return a - b;
+        }
+
+        private static double SimpleSummary(int deltaBtwMonths, double socialNormEqualTarif)
+        {
+            return deltaBtwMonths * socialNormEqualTarif;
+        }
+
+        private static double DifficultSummary(int deltaBtwMonths, double socialNormEqualTarif, int deltaBtwNorms,
+            double socialNormNotEqualTarif)
+        {
+            return deltaBtwNorms * socialNormNotEqualTarif + deltaBtwMonths * socialNormEqualTarif;
+        }
+
+        public double FindPhaseSummary(int deltaBtwMonths, int socialNorm, double socialNormEqualTarif, double socialNormNotEqualTarif)
+        {
+            var deltaBtwNorms = CreateDelta(deltaBtwMonths, socialNorm);
+
+            if (deltaBtwNorms <= 0) return SimpleSummary(deltaBtwMonths, socialNormEqualTarif);
+            return DifficultSummary(deltaBtwMonths, socialNormEqualTarif, deltaBtwNorms, socialNormNotEqualTarif);
+        }
+
+        public double ResultSummary(double daySummary, double nightSummary)
+        {
+            return daySummary + nightSummary;
+        }
+    }
+}
