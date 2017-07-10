aae92bed0b0fc890f730a41e4f6a0fbf9365f6bc
stanislav-okunev <Stanislav.Okunev@returnonintelligence.com>
2017-07-11 01:36:21 +0300

    some calculation (manual testing)

--- ElectricityPayments/Program.cs
+++ ElectricityPayments/Program.cs
@@ -7,12 +7,15 @@ namespace ElectricityPayments
         private static void Main()
         {
             var calculator = new ServiceCalc();
+            var jule = new Month(2376, 1644);
             var june = new Month(2342, 1625);
             var may = new Month(2160, 1521);
 
-            var summary = calculator.SummaryResult(june, may);
+            var summary1 = calculator.SummaryResult(june, may);
+            var summary2 = calculator.SummaryResult(jule, june);
 
-            Console.WriteLine("Your result is: " + summary);
+            Console.WriteLine("Your previous result is: " + summary1);
+            Console.WriteLine("Your current result is: " + summary2);
             Console.ReadKey();
         }
     }
